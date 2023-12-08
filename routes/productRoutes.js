c332db36a92dfeda43465cc5a5ac40dbb3ec824f
Hussein Mohamed <114592524+Hussein-ElMorsy@users.noreply.github.com>
2023-12-08 14:13:11 +0200

    Protect product and review routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const productController = require('./../controllers/productController');
-//const authController = require('./../controllers/authController');
-//const reviewRouter = require('./reviewRoutes');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:productId/reviews', reviewRouter);
 
 router
   .route('/')
   .get(productController.getAllProducts)
-  .post(productController.createProduct);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin'),
+    productController.createProduct,
+  );
 
 router
   .route('/:id')
   .get(productController.getProduct)
-  .patch(productController.updateProduct)
-  .delete(productController.deleteProduct);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    productController.updateProduct,
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    productController.deleteProduct,
+  );
 
 module.exports = router;
